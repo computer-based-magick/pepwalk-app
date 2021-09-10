4f39a16fbec036a269e03e09532d2cfe09984366
Andy19936 <andy19936@gmail.com>
2021-09-10 11:52:35 -0700

    add missing files

---
+++ app/javascript/components/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Hero from "../components/Hero";
+
+const NotFound = () => {
+  return (
+    <>
+      <h4>Page not found</h4>
+    </>
+  );
+};
+
+export default NotFound;

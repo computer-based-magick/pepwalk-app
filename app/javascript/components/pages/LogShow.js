90e75f3f762724cfb97c1deb7f236d596d801d26
Andy19936 <andy19936@gmail.com>
2021-09-13 09:48:47 -0700

    added back  button to log page

--- app/javascript/components/pages/LogShow.js
+++ app/javascript/components/pages/LogShow.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useParams } from "react-router";
+import { Link } from "react-router-dom";
 
 export const LogShow = ({ log }) => {
   console.log("LOG!!!", log);
@@ -37,6 +37,14 @@ export const LogShow = ({ log }) => {
           >
             edit
           </button>
+          <Link to="/logs">
+            <button
+              type="button"
+              className="border border-green-500 text-green-500 rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:text-white hover:bg-green-600 focus:outline-none focus:shadow-outline"
+            >
+              back to your logs
+            </button>
+          </Link>
           <button
             type="button"
             className="border border-red-500 text-red-500 rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:text-white hover:bg-red-600 focus:outline-none focus:shadow-outline"

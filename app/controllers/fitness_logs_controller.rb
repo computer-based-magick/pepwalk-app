afe52823754d198998a4614a6fa732974142e9e2
hroulston <h_roulston@live.com>
2021-09-13 14:35:28 -0700

    add endpoint for create

--- app/controllers/fitness_logs_controller.rb
+++ app/controllers/fitness_logs_controller.rb
@@ -5,4 +5,17 @@ class FitnessLogsController < ApplicationController
     render json: fitnesslogs
   end
 
+  def create
+    fitnesslog = FitnessLog.create(log_params)
+    if fitnesslog.valid?
+      render json:fitnesslog
+    else
+      render json: fitnesslog.errors, status: 422
+    end
+  end
+
+  private
+    def log_params
+      params.require(:fitnesslog).permit(:date, :workout_name, :happy, :sad, :energetic, :email, :lethargic, :entry)
+    end
 end

e9f25e66a3f42575ba3cb4d1d5948cd3161ded04
Jared Thomas <85851208+Jatded1d9l7@users.noreply.github.com>
2021-09-14 19:35:12 -0400

    validations for fitness log

--- app/models/fitness_log.rb
+++ app/models/fitness_log.rb
@@ -1,3 +1,4 @@
 class FitnessLog < ApplicationRecord
     belongs_to :user
+    validates :date, :workout_id, :workout_name, :happy, :sad, :energetic, :lethargic, :entry, :user_id, presence: true
 end

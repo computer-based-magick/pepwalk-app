100e9dff6304ccf0f0dd11e90fb4f5f50eb6632c
Jared Thomas <85851208+Jatded1d9l7@users.noreply.github.com>
2021-09-15 14:02:39 -0400

    testing for about and Addlog

---
+++ app/javascript/components/pages/__tests__/AddLog.test.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Enzyme, { shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import AddLog from "../AddLog";
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe("When Logs page renders", () => {
+  const logs = [
+      {
+          date: "2021-09-10T22:09:16.408Z",
+          workout_id: 874,
+          workout_name: 'Forward Lunge',
+          happy: 5,
+          sad: 5,
+          energetic: 7,
+          lethargic: 2,
+          entry: 'crushed it!',
+          id: 1
+      }
+  ]
+
+  it("has a button that says submit", () => {
+    const renderlogs = shallow(<AddLog logs={logs}/>);
+    const renderText = renderlogs.find("button");
+    expect(renderText.text()).toEqual("Submit");
+  });
+});

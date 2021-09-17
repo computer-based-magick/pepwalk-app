a7939367dd04a505510be6903b89280d093338cd
Andy19936 <andy19936@gmail.com>
2021-09-17 16:06:04 -0700

    working data vis with log info

--- app/javascript/components/pages/BarChart.js
+++ app/javascript/components/pages/BarChart.js
@@ -1,63 +1,71 @@
-import React from 'react'
-import { Bar } from 'react-chartjs-2'
+import React from "react";
+import { Bar } from "react-chartjs-2";
+import { format } from "date-fns";
 
 const BarChart = ({ logs }) => {
-    return (
-      <div>
-      {logs && logs.map((log) => {
-        return (
-      < Bar
-        data= {{
-          labels: ['sad', 'happy', 'energetic', 'lethargic'],
-          datasets: [
-            {
-              label: 'Day',
-              data: [12, 19, 3, 2],
-              backgroundColor: [
-                'rgba(255, 99, 132, 0.5)',
-                'rgba(54, 162, 235, 0.5)',
-                'rgba(255, 206, 86, 0.5)',
-                'rgba(75, 192, 192, 0.5)',
-              ],
-              borderColor: [
-              'rgba(255, 99, 132, 1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)',
-              'rgba(75, 192, 192, 1)',
-            ],
-            borderWidth: 1,
-            },
-            {
-              label: 'week',
-              data: [8, 12, 5, 14],
-              backgroundColor: [
-                'rgba(75, 192, 192, 0.5)',
-                'rgba(255, 206, 86, 0.5)',
-                'rgba(54, 162, 235, 0.5)',
-                'rgba(255, 99, 132, 0.5)',
-              ]
-            }
-          ],
-        }}
-        height= {400}
-        width= {600}
-        options={{
-          mainAspectRatio: false,
-          scales: {
-            yAxis: [
-              {
-                ticks: {
-                  beginAtZero: true,
-                }
-              }
-            ]
-          }
-        }}
-      />
-      </div>
-    )
-  )
-}
-  }
+  console.log({ logs });
+  return (
+    <div>
+      {logs &&
+        logs.map((log) => {
+          return (
+            <Bar
+              data={{
+                labels: ["sad", "happy", "energetic", "lethargic"],
+                datasets: [
+                  {
+                    label: `${format(new Date(log.date), "MMMM dd yy")}`,
+                    data: [
+                      `${log.sad}`,
+                      `${log.happy}`,
+                      `${log.energetic}`,
+                      `${log.lethargic}`,
+                    ],
+                    backgroundColor: [
+                      "rgba(255, 99, 132, 0.5)",
+                      "rgba(54, 162, 235, 0.5)",
+                      "rgba(255, 206, 86, 0.5)",
+                      "rgba(75, 192, 192, 0.5)",
+                    ],
+                    borderColor: [
+                      "rgba(255, 99, 132, 1)",
+                      "rgba(54, 162, 235, 1)",
+                      "rgba(255, 206, 86, 1)",
+                      "rgba(75, 192, 192, 1)",
+                    ],
+                    borderWidth: 1,
+                  },
+                  // {
+                  //   label: "week",
+                  //   data: [8, 12, 5, 14],
+                  //   backgroundColor: [
+                  //     "rgba(75, 192, 192, 0.5)",
+                  //     "rgba(255, 206, 86, 0.5)",
+                  //     "rgba(54, 162, 235, 0.5)",
+                  //     "rgba(255, 99, 132, 0.5)",
+                  //   ],
+                  // },
+                ],
+              }}
+              height={100}
+              width={300}
+              options={{
+                mainAspectRatio: false,
+                scales: {
+                  yAxis: [
+                    {
+                      ticks: {
+                        beginAtZero: true,
+                      },
+                    },
+                  ],
+                },
+              }}
+            />
+          );
+        })}
+    </div>
+  );
+};
 
-export default BarChart
+export default BarChart;
